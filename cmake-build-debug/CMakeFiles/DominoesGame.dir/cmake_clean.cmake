8287904e33be1d8fa7c495c10f29a1f14c573de0
McDonaldRo <37886586+McDonaldRo@users.noreply.github.com>
2018-04-15 15:04:26 -0700

    Made 6_interface_newOBJpointer.cpp main and changed # drawn from 12 to 10
    (1) Set "6_interface_newOBJpointer.cpp" as main, deleting other files.
    (2) Created CMake/build files in CLion
    (3) Replaced instances of "srandom" with "srand"
    (4) Changed number of dominoes drawn at start from 12 to 10 for each player

---
+++ cmake-build-debug/CMakeFiles/DominoesGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DominoesGame.dir/6_interface_newOBJpointer.cpp.obj"
+  "DominoesGame.pdb"
+  "DominoesGame.exe"
+  "DominoesGame.exe.manifest"
+  "libDominoesGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DominoesGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
